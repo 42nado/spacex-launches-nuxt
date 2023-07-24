0936d3ae35718c6e7475f141325477866a55f133
42nado <daryllfortunado@gmail.com>
2023-07-24 10:42:50 +0800

    feat: component for rocket

---
+++ client/components/RocketCard.vue
@@ -0,0 +1,28 @@
+<template>
+	<v-card class="mx-auto my-4" max-width="374">
+		<v-card-title class="font-weight-bold">{{ rocket.name }}</v-card-title>
+		<v-card-text>
+			<div class="my-4 text-subtitle-1">
+				<span class="font-weight-bold">First Flight Date :</span>
+				{{ rocket.first_flight }}
+			</div>
+			<v-divider class="mx-4" />
+			<div>
+				{{ rocket.description }}
+			</div>
+		</v-card-text>
+		<v-card-actions>
+			<NuxtLink :to="`/rockets/${rocket.id}`">
+				<v-btn color="deep-purple lighten-2">More Details</v-btn>
+			</NuxtLink>
+			<v-btn color="deep-purple lighten-2">Favorites</v-btn>
+		</v-card-actions>
+	</v-card>
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue'
+
+const props = defineProps(['r'])
+const rocket = ref(props.r)
+</script>
--- client/pages/rockets/index.vue
+++ client/pages/rockets/index.vue
@@ -1,28 +1,9 @@
 <template>
 	<v-container>
 		<h1 class="text-weight-bold text-center">There are {{ rockets?.length || 0 }} rockets.</h1>
-
 		<v-col no-gutters>
 			<v-row>
-				<v-card v-for="rocket in rockets" :key="rocket.name" class="mx-auto my-4" max-width="374">
-					<v-card-title class="font-weight-bold">{{ rocket.name }}</v-card-title>
-					<v-card-text>
-						<div class="my-4 text-subtitle-1">
-							<span class="font-weight-bold">First Flight Date :</span>
-							{{ rocket.first_flight }}
-						</div>
-						<v-divider class="mx-4" />
-						<div>
-							{{ rocket.description }}
-						</div>
-					</v-card-text>
-					<v-card-actions>
-						<NuxtLink :to="`/rockets/${rocket.id}`">
-							<v-btn color="deep-purple lighten-2">More Details</v-btn>
-						</NuxtLink>
-						<v-btn color="deep-purple lighten-2">Favorites</v-btn>
-					</v-card-actions>
-				</v-card>
+				<RocketCard v-for="rocket in rockets" :key="rocket.name" :r="rocket" />
 			</v-row>
 		</v-col>
 	</v-container>
